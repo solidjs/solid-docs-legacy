c72c7583b06a4b48cba2d7b99cd7851cf84d7f94
modderme123 <modderme123@gmail.com>
2022-07-10 14:21:32 -0700

    don't throw on unknown import

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,27 @@ import { DocFile, LessonFile, LessonLookup, ResourceMetadata } from "./types";
 
 export { DocFile, LessonFile, LessonLookup, ResourceMetadata };
 
-export async function getApi(
-  lang: string
-): Promise<DocFile | undefined> {
-  return (await import(`../langs/${lang}/api/api.md`));
+function noThrow<T>(x: Promise<T>): Promise<T | undefined> {
+  return x.catch(() => undefined);
+}
+
+export async function getApi(lang: string): Promise<DocFile | undefined> {
+  return await noThrow(import(`../langs/${lang}/api/api.md`));
 }
 
 export async function getDoc(
   lang: string,
   resource?: string
 ): Promise<DocFile | undefined> {
-  return (await import(`../langs/${lang}/guides/${resource}.md`));
+  return await noThrow(import(`../langs/${lang}/guides/${resource}.md`));
 }
 
 export async function getGuideDirectory(
   lang: string
 ): Promise<ResourceMetadata[] | undefined> {
-  const directory = await import(`../langs/${lang}/guides/directory.json`);
+  const directory = await noThrow(
+    import(`../langs/${lang}/guides/directory.json`)
+  );
   return directory?.default;
 }
 
@@ -27,20 +31,22 @@ export async function getTutorial(
   lesson: string
 ): Promise<LessonFile | undefined> {
   const [lessonFiles, solved, markdown] = await Promise.all([
-    import(`../langs/${lang}/tutorials/${lesson}/lesson.json`),
-    import(`../langs/${lang}/tutorials/${lesson}/solved.json`),
-    import(`../langs/${lang}/tutorials/${lesson}/lesson.md`),
+    noThrow(import(`../langs/${lang}/tutorials/${lesson}/lesson.json`)),
+    noThrow(import(`../langs/${lang}/tutorials/${lesson}/solved.json`)),
+    noThrow(import(`../langs/${lang}/tutorials/${lesson}/lesson.md`)),
   ]);
   return {
-    lesson: lessonFiles.default,
-    solved: solved.default,
-    markdown: markdown.default,
+    lesson: lessonFiles?.default,
+    solved: solved?.default,
+    markdown: markdown?.default,
   };
 }
 
 export async function getTutorialDirectory(
   lang: string
 ): Promise<LessonLookup[] | undefined> {
-  const directory = await import(`../langs/${lang}/tutorials/directory.json`);
+  const directory = await noThrow(
+    import(`../langs/${lang}/tutorials/directory.json`)
+  );
   return directory?.default;
 }
