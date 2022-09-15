30e1092a65592fca36901371778364f90e62888f
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2022-09-15 10:25:44 +0200

    Generate JSON files from "/examples-src/" to "/examples/"

--- src/json-files/rollup-plugin.js
+++ src/json-files/rollup-plugin.js
@@ -6,47 +6,87 @@ import glob from 'glob';
 import { dirname, parse, basename } from 'path';
 import { readFileSync, writeFileSync } from 'fs';
 
+const pluginName = 'generate-json-folders';
+
+const directoryPattern = '**/examples-src/$descriptor.json';
+const examplesPattern = '**/examples-src/*/$descriptor.json';
+
+function getGlobPaths(...patterns) {
+  return glob.sync(
+    patterns.length > 1 ? `{${patterns.join(',')}}` : patterns[0], 
+    {absolute: true}
+  );
+}
+
+function getDescriptor(descriptorPath) {
+  let descriptor = null;
+  try {
+    descriptor = JSON.parse(readFileSync(descriptorPath, 'utf8'));
+  } catch (e) {
+    console.warn(`[${pluginName}] skipping ${descriptorPath} because ${e}`);
+    return descriptor;
+  }
+  return descriptor;
+}
+
+function getGeneratedPath(path) {
+  return path.replace('/examples-src/', '/examples/');
+}
+
+function getFiles(folderPath, filenames) {
+  return (filenames || []).map((filename) => {
+    const filepath = `${folderPath}/${filename}`;
+    const {name, ext} = parse(filename);
+    return {
+      name, 
+      type: ext.slice(1), 
+      content: readFileSync(filepath, 'utf8')
+    };
+  });
+}
+
+function createExamplesDirectory(cachedExamples) {
+  const descriptorPaths = glob.sync(directoryPattern, {absolute: true});
+  descriptorPaths.forEach((descriptorPath) => {
+    const descriptor = getDescriptor(descriptorPath);
+    if (!descriptor) return; // skip
+    const directory = descriptor.map((id) => {
+      const exampleSrcPath = `${dirname(descriptorPath)}/${id}`;
+      const example = cachedExamples[exampleSrcPath];
+      return example;
+    });
+    const directoryPath = getGeneratedPath(descriptorPath);
+    writeFileSync(directoryPath, JSON.stringify(directory));
+  });
+}
+
 export default function pack(pluginOptions) {
-  const pluginName = 'json-files';
   if (pluginOptions) {
     console.warn(`[${pluginName}] no plugin options are supported`);
   }
   return {
     name: pluginName,
     buildStart() {
-      const matchers = '{**/examples/*/_index.json,**/tutorials/*/*/_index.json}';
-      const allPaths = glob.sync(matchers, {absolute: true});
-      allPaths.forEach((descriptorPath) => {
-        let descriptor = {};
-        try {
-          descriptor = JSON.parse(readFileSync(descriptorPath, 'utf8'));
-        } catch (e) {
-          console.warn(`[${pluginName}] skipping ${descriptorPath} because ${e}`);
-          return;
-        }
-        if (!Array.isArray(descriptor.files) || descriptor.files.length === 0) {
-          console.warn(`[${pluginName}] skipping ${descriptorPath} because the files property is not a list or it's an empty list`);
-          return;
-        }
+      const descriptorPaths = getGlobPaths(examplesPattern);
+      const cachedExamples = {};
+      descriptorPaths.forEach((descriptorPath) => {
+        const descriptor = getDescriptor(descriptorPath);
+        if (!descriptor) return; // skip
         
-        const examplePath = dirname(descriptorPath);
-        const outputFilename = `${examplePath}.json`;
-        const files = descriptor.files.map((filename) => {
-          const inputFilename = `${examplePath}/${filename}`;
-          const parsedFilename = parse(filename);
-          return {
-            name: parsedFilename.name, 
-            type: parsedFilename.ext.slice(1), 
-            content: readFileSync(inputFilename, 'utf8')
-          };
-        });
-        writeFileSync(outputFilename, JSON.stringify({
-          id: basename(examplePath),
+        const exampleSrcPath = dirname(descriptorPath);
+        const files = getFiles(exampleSrcPath, descriptor.files);
+        const example = {
+          id: basename(exampleSrcPath),
           name: descriptor.name,
           description: descriptor.description,
           files
-        }));
+        };
+        const examplePath = getGeneratedPath(exampleSrcPath);
+        writeFileSync(`${examplePath}.json`, JSON.stringify(example));
+        delete example.files;
+        cachedExamples[exampleSrcPath] = example;
       });
+      createExamplesDirectory(cachedExamples);
     }
   };
 }
